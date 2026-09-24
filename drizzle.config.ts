d5b726f2cec180f44d2aeab7242af68495f6c606
Tyler Thibault <tyler.thibault@protonmail.com>
2026-09-24 09:12:23 -0700

    Creative Circle: add drizzle.config.ts

---
+++ drizzle.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+  schema: "./src/lib/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: { url: process.env.DATABASE_URL ?? "postgres://postgres:postgres@localhost:5432/creative_circle" }
+});
